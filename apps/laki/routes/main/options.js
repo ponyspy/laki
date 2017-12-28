9b20eb945acf2ea229572a847a6d5bab4537221b
ponyspy <desade4me@gmail.com>
2017-12-28 20:27:18 +0300

    sitemap

--- apps/laki/routes/main/options.js
+++ apps/laki/routes/main/options.js
@@ -4,31 +4,41 @@ module.exports = function(Model) {
 	var module = {};
 
 	var Collect = Model.Collect;
+	var Direction = Model.Direction;
 
 	module.sitemap = function(req, res, next) {
+
 		Collect.where('status').ne('hidden').exec(function(err, collects) {
-			arr_collects = collects.map(function(collect) {
-				return {
-					url: '/collections/' + collect._short_id
-				};
+			Direction.where('status').ne('hidden').exec(function(err, directions) {
+				var arr_directions = directions.map(function(direction) {
+					return {
+						url: '/directions/' + direction._short_id
+					};
+				});
+
+				var arr_collects = collects.map(function(collect) {
+					return {
+						url: '/collections/' + collect._short_id
+					};
+				});
+
+				var site_map = sitemap.createSitemap ({
+					hostname: 'https://' + req.hostname,
+					// cacheTime: 600000,
+					urls: [
+						{ url: '/' },
+						{ url: '/concept' },
+						{ url: '/buy' },
+					].concat(arr_directions).concat(arr_collects)
+				});
+
+				site_map.toXML(function (err, xml) {
+					if (err) return next(err);
+
+					res.type('xml').send(xml);
+				});
+
 			});
-
-			var site_map = sitemap.createSitemap ({
-				hostname: 'https://' + req.hostname,
-				// cacheTime: 600000,
-				urls: [
-					{ url: '/' },
-					{ url: '/concept' },
-					{ url: '/buy' },
-				].concat(arr_collects)
-			});
-
-			site_map.toXML(function (err, xml) {
-				if (err) return next(err);
-
-				res.type('xml').send(xml);
-			});
-
 		});
 	};
 
