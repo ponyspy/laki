f502b61a997db06814f7669957bb521cafc9549f
ponyspy <desade4me@gmail.com>
2018-01-24 15:54:50 +0300

    blog collects

--- apps/laki/routes/main/blog.js
+++ apps/laki/routes/main/blog.js
@@ -4,12 +4,17 @@ module.exports = function(Model) {
 	var module = {};
 
 	var Post = Model.Post;
+	var Collect = Model.Collect;
 
 	module.index = function(req, res, next) {
 		Post.find().where('status').ne('hidden').sort('-date').exec(function(err, posts) {
 			if (err) return next(err);
 
-			res.render('main/blog.jade', { posts: posts });
+			Collect.find().where('status').ne('hidden').exec(function(err, collects) {
+				if (err) return next(err);
+
+				res.render('main/blog.jade', { posts: posts, collects: collects });
+			});
 		});
 	};
 
@@ -17,7 +22,7 @@ module.exports = function(Model) {
 		var id = req.params.short_id;
 
 		Post.findOne({ $or: [ { '_short_id': id }, { 'sym': id } ] }).where('status').ne('hidden').exec(function(err, post) {
-			if (err) return next(err);
+		if (err) return next(err);
 
 			res.render('main/post.jade', { post: post });
 		});
