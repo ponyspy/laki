d1f17c738bcd7ddd1577ff6dc9111a603e12d3a3
ponyspy <desade4me@gmail.com>
2018-01-28 20:00:39 +0300

    fix sample

--- apps/laki/routes/main/collects.js
+++ apps/laki/routes/main/collects.js
@@ -17,7 +17,7 @@ module.exports = function(Model, Params) {
 		Collect.findOne({ $or: [ { '_short_id': id }, { 'sym': id } ] }).where('status').ne('hidden').exec(function(err, collect) {
 			if (err) return next(err);
 
-			Collect.aggregate([{ $sample: { size: 3 } }]).exec(function(err, sim_items) {
+			Collect.aggregate([{ $match : { _id : { $ne: collect._id } } }, { $sample: { size: 3 } }]).exec(function(err, sim_items) {
 				if (err) return next(err);
 
 				res.render('main/collect.jade', { collect: collect, sim_items: sim_items, get_locale: get_locale });
