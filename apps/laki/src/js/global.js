4de4c7a2234cf7b261e12f068f18b6b331f48c57
ponyspy <desade4me@gmail.com>
2018-01-30 14:36:27 +0300

    menu

--- apps/laki/src/js/global.js
+++ apps/laki/src/js/global.js
@@ -1,4 +1,7 @@
 $(function() {
+	$(document)
+		.on('touchmove', 'body.menu_open', false);
+
 	$('.menu_drop').on('click', function(e) {
 		$(this).toggleClass('open');
 		$('body, .maket_block').toggleClass('menu_open');
