e19e52f8aea0f004e7f580a7dce1144aa92af883
ponyspy <desade4me@gmail.com>
2018-02-04 19:44:33 +0300

    sort

--- apps/laki/routes/main/index.js
+++ apps/laki/routes/main/index.js
@@ -5,10 +5,10 @@ module.exports = function(Model) {
 	var Collect = Model.Collect;
 
 	module.index = function(req, res, next) {
-		Direction.where('status').ne('hidden').populate('collects').exec(function(err, directions) {
+		Direction.where('status').ne('hidden').sort('-date').populate('collects').exec(function(err, directions) {
 			if (err) return next(err);
 
-			Collect.where('main_slider').equals('true').exec(function(err, collects) {
+			Collect.where('main_slider').equals('true').sort('-date').exec(function(err, collects) {
 				if (err) return next(err);
 
 				res.render('main/index.jade', { directions: directions, collects: collects });
